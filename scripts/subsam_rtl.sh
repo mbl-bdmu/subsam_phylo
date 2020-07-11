a50f2fb3cfd33a856d0c5248e4691e07795ed47e
RITM Molecular Biology Lab <ritm.molbiolabresearch@gmail.com>
2020-07-11 13:24:26 +0800

    Link to Treemmer_v0.3.py expected in /usr/bin at runtime

--- scripts/subsam_rtl.sh
+++ scripts/subsam_rtl.sh
@@ -4,7 +4,7 @@ OUTNAME=$2
 RTLNUM=$3
 CPUNUM=$4
 
-python3 /mnt/c/Users/Guest1/fgmp_working_dir/apps/Treemmer/Treemmer_v0.3.py $NWKFILE -RTL $RTLNUM --cpu $CPUNUM --verbose 1 --plot_always &&
+python3 /usr/bin/Treemmer_v0.3.py $NWKFILE -RTL $RTLNUM --cpu $CPUNUM --verbose 1 --plot_always &&
 mv ${NWKFILE}_trimmed_list_RTL_${RTLNUM} $OUTNAME
 
 ## ADD OPTION TO PROTECT SPECIFIC HEADERS FROM TRIM
